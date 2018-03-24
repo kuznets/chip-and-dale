7a12333f59d39d90e7b1c5245a90f15348a6c56e
Roman Vlasov <89101819710rv@mail.com>
2018-03-24 21:29:30 +0300

    недолив

---
+++ src/app/profile/profile-detail-edit/profile-detail-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-detail-edit',
+  templateUrl: './profile-detail-edit.component.html',
+  styleUrls: ['./profile-detail-edit.component.scss']
+})
+export class ProfileDetailEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -4,11 +4,25 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileWidgetComponent } from './profile-widget/profile-widget.component';
 import { ProfileOrdersComponent } from './profile-orders/profile-orders.component';
+import {ProfileDetailComponent} from "./profile-detail/profile-detail.component";
+import {ProfileDetailEditModule} from "./profile-detail-edit/profile-detail-edit.module";
 
 @NgModule({
   imports: [
     CommonModule,
+    ProfileModule,
+    ProfileDetailComponent,
+    ProfileDetailEditModule,
+    ProfileOrdersComponent,
+    ProfileWidgetComponent,
     ProfileRoutingModule
+  ],
+  exports: [
+    ProfileModule,
+    ProfileDetailComponent,
+    ProfileDetailEditModule,
+    ProfileOrdersComponent,
+    ProfileWidgetComponent
   ],
   declarations: [ProfileWidgetComponent, ProfileOrdersComponent]
 })
