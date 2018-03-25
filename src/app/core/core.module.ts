610444011e955356ab9f4e976f2de0a77923cb0f
Sergei R <sresetnjak@gmail.com>
2018-03-25 10:37:22 +0300

    Add register module. Auth module move to core.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MaterialLoaderModule} from "./material-loader/material-loader.module";
+import { MaterialLoaderModule } from './material-loader/material-loader.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialLoaderModule
+    MaterialLoaderModule,
+    AuthModule
   ],
   exports: [
-    MaterialLoaderModule
+    MaterialLoaderModule,
+    AuthModule
   ],
   declarations: [
 
