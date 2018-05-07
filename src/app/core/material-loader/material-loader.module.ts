0bb19521e66a668f2ddfb1d41946ade77ad6fce1
Sergei R <1sttech.eu@gmail.com>
2018-05-07 19:23:16 +0300

    Change imports for material components.

--- src/app/core/material-loader/material-loader.module.ts
+++ src/app/core/material-loader/material-loader.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MatButtonModule,
-  MatFormFieldModule,
-  MatToolbarModule,
-  MatMenuModule,
-  MatIconModule,
-  MatGridListModule,
-  MatListModule,
-  MatCardModule,
-  MatPaginatorModule,
-  MatSelectModule,
-  MatTableModule,
-  MatDialogModule,
-  MatInputModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [
